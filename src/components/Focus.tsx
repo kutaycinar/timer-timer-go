3a66df8238b04618cde1f304894c97be5fb7ca0b
Cobey Hollier <cobey.hollier@mac.com>
2023-02-23 21:09:12 +0100

    CH: Fixing verticle alignment of elements

--- src/components/Focus.tsx
+++ src/components/Focus.tsx
@@ -32,34 +32,35 @@ function Focus({
 
   return (
     <div className="container">
-      <svg>
+      <button onClick={() => signalStop()} className="back-button">
+        <FaChevronLeft />
+      </button>
+      <h3 className="title">{name}</h3>
+      <svg style={{ width: "0px", height: "0px" }}>
         <defs>
-          <linearGradient id="testing" x1="1" y1="0" x2="0" y2="0">
+          <linearGradient id="gradient" x1="1" y1="0" x2="0" y2="0">
             <stop offset="5%" stopColor="#24DBE4" />
             <stop offset="95%" stopColor="#5681F6" />
           </linearGradient>
         </defs>
       </svg>
-      <button onClick={() => signalStop()} className="back-button">
-        <FaChevronLeft />
-      </button>
-      <h3 className="title">{name}</h3>
       <CountdownCircleTimer
         key={key}
         isPlaying={isRunning}
         duration={total || 0}
         initialRemainingTime={delta}
-        colors={"url(#testing)"}
+        colors={"url(#gradient)"}
         size={300}
         strokeWidth={15}
         trailStrokeWidth={30}
         trailColor="#292660"
       >
         {({ remainingTime, color = "A30000" }) => (
-          <h1 color={color}>{parseTime(remainingTime)} </h1>
+          <h1 className="timer-text" color={color}>
+            {parseTime(remainingTime)}{" "}
+          </h1>
         )}
       </CountdownCircleTimer>
-
       <div className="button-container">
         <button
           className="play-button"
@@ -68,7 +69,6 @@ function Focus({
           {isRunning ? <FaPause /> : <FaPlay />}
         </button>
       </div>
-
       <div className="footer-buttons">
         <button
           className="action-button"
