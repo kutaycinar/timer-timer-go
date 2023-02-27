e08c73be8d44e02c1d8be6bcb27c36538fe078f1
Cobey Hollier <cobey.hollier@mac.com>
2023-02-27 10:07:40 +0100

    CH: Fixing merge issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Modal from "./components/Add";
 import Focus from "./components/Focus";
 import Overview from "./components/Overview";
+import ThemeProvider from "./components/ThemeProvider";
 import Timer from "./components/Timer";
 import BrowserWrapper from "./components/Wrapper";
 import { useTimer } from "./hooks";
@@ -24,40 +25,42 @@ function App() {
 
   return (
     <BrowserWrapper>
-      {/* <pre>{JSON.stringify(state, undefined, 2)}</pre> */}
-      {state.state.focus !== -1 ? (
-        <div>
-          <Focus
-            {...state.state.timers[state.state.focus]}
-            signalStart={signalStart}
-            signalPause={signalPause}
-            signalStop={signalStop}
-            signalReset={signalReset}
-            isRunning={state.state.active}
-            countNext={countNext}
-            editTimer={editTimer}
-            deleteTimer={deleteTimer}
-          />
-        </div>
-      ) : (
-        <div className="page">
-          <Overview {...getOverall()} />
-          {state.state.timers.map((t, idx) => (
-            <Timer
-              key={t.name}
-              {...t}
-              idx={idx}
+      <ThemeProvider>
+        {/* <pre>{JSON.stringify(state, undefined, 2)}</pre> */}
+        {state.state.focus !== -1 ? (
+          <div>
+            <Focus
+              {...state.state.timers[state.state.focus]}
+              signalStart={signalStart}
+              signalPause={signalPause}
+              signalStop={signalStop}
+              signalReset={signalReset}
+              isRunning={state.state.active}
+              countNext={countNext}
+              editTimer={editTimer}
               deleteTimer={deleteTimer}
-              focusTimer={focusTimer}
             />
-          ))}
-          <Modal setHook={addTimer} reset={true}>
-            <button className="add">
-              <FaPlus />
-            </button>
-          </Modal>
-        </div>
-      )}
+          </div>
+        ) : (
+          <div className="page">
+            <Overview {...getOverall()} />
+            {state.state.timers.map((t, idx) => (
+              <Timer
+                key={t.name}
+                {...t}
+                idx={idx}
+                deleteTimer={deleteTimer}
+                focusTimer={focusTimer}
+              />
+            ))}
+            <Modal setHook={addTimer} reset={true}>
+              <button className="add">
+                <FaPlus />
+              </button>
+            </Modal>
+          </div>
+        )}
+      </ThemeProvider>
     </BrowserWrapper>
   );
 }
