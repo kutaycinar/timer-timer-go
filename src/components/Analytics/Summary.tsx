58e2ff02ee41a636a31003b6fea1c72b31b4df06
Kutay Cinar <kutaycinar@uvic.ca>
2023-03-12 21:19:01 +0300

    week day font size

--- src/components/Analytics/Summary.tsx
+++ src/components/Analytics/Summary.tsx
@@ -1,21 +1,21 @@
-import dayjs from "dayjs";
-import { CircularProgressbarWithChildren } from "react-circular-progressbar";
-import { Save } from "../../types";
+import dayjs from "dayjs"
+import { CircularProgressbarWithChildren } from "react-circular-progressbar"
+import { Save } from "../../types"
 
 function Summary({ data }: { data: Save[] }) {
-  const today = dayjs();
-  let lastWeek: Save[] = [];
+  const today = dayjs()
+  let lastWeek: Save[] = []
   for (let i = 7; i >= 1; i--) {
-    const date = today.subtract(i, "days");
-    const save = data.find((s: Save) => dayjs(s.date).isSame(date, "day"));
+    const date = today.subtract(i, "days")
+    const save = data.find((s: Save) => dayjs(s.date).isSame(date, "day"))
     if (save) {
-      lastWeek.push(save);
+      lastWeek.push(save)
     } else {
       lastWeek.push({
         completion: 0,
         date: date.valueOf(),
-        timers: [], //TODO: Backfill timers from previous day with progress
-      });
+        timers: [],
+      })
     }
   }
 
@@ -30,13 +30,13 @@ function Summary({ data }: { data: Save[] }) {
     >
       {lastWeek.map((save: Save, idx: number) => (
         <CircularProgressbarWithChildren value={save.completion} key={idx}>
-          <h6 style={{ marginTop: "32px" }}>
+          <div className="week-title">
             {dayjs(save.date).format("ddd")}
-          </h6>
+          </div>
         </CircularProgressbarWithChildren>
       ))}
     </div>
-  );
+  )
 }
 
-export default Summary;
+export default Summary
