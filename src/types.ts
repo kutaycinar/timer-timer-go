c7bc3fedb22080450bb32ac42311419289817578
Cobey Hollier <cobey.hollier@mac.com>
2023-03-13 15:49:26 +0100

    CH: Removing unused empty data

--- src/types.ts
+++ src/types.ts
@@ -30,26 +30,13 @@ export type Save = {
 
 export type themeOption = "dark" | "light";
 
-const emptyList: Save[] = [];
-for (let index = 1; index < 9; index++) {
-  const empty: Save = {
-    date: new Date(
-      new Date().getTime() - index * (24 * 60 * 60 * 1000)
-    ).getTime(),
-    timers: [],
-    completion: 0,
-  };
-  emptyList.push(empty);
-}
-emptyList.reverse();
-
 export const initial: State = {
   state: {
     date: dayjs().valueOf(),
     active: false,
     focus: -1,
     timers: [],
-    saves: emptyList,
+    saves: [],
     promode: false,
   },
 };
